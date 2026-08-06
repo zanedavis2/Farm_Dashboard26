685808fa00dde10a94976b868c22023de3942daa
Zane Davis <zdavis@Zanes-MacBook-Air.local>
2026-08-06 12:58:23 -0400

    Final touches v2

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
 from datetime import date
+import os
 
 PROJECT_ID = "farm-dashboard-502817"
 
 DATASET_ID = "farm_sample_dataset"
 
-CREDENTIALS_PATH = "credentials/farm-dashboard-502817-4baf1e1a6668.json"
+##CREDENTIALS_PATH = "credentials/farm-dashboard-502817-4baf1e1a6668.json"
+
+CREDENTIALS_PATH = os.getenv(
+    "GOOGLE_APPLICATION_CREDENTIALS",
+    "credentials.json"
+)
 
 SALES_TABLE = "sales"
 
